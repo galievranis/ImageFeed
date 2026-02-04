6bb203226ee9b9c254005803fc473c59334813a8
Ranis Galiev <galievdesign@gmail.com>
2026-02-04 09:14:43 +0500

    feat: add user profile screen layout

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Ranis Galiev on 04.02.2026.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
